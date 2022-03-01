7e4ebeb24dbdcd83f6b30787efa5668beeca957d
Tamás Benke <tombenke@gmail.com>
2022-03-01 15:51:50 +0100

    Document the wsgw CLI tools Upgrade packages

--- src/commands/consumer/index.js
+++ src/commands/consumer/index.js
@@ -18,14 +18,14 @@ exports.execute = (container, args, responseCb) => {
     if (args.channelType === 'NATS') {
         container.logger.info(`Start listening to messages on NATS "${args.topic}" topic`)
         container.pdms.add({ pubsub$: true, topic: args.topic }, (data) => {
-            container.logger.info(`[${args.topic}] >> ${JSON.stringify(data)}\n`)
+            container.logger.info(`NATS[${args.topic}] >> ${JSON.stringify(data)}\n`)
         })
     } else {
         container.logger.info(`Start listening to messages on WebSocket "${args.topic}" topic`)
         const wsClient = ioClient(serverUri)
 
         wsClient.on(args.topic, (data) => {
-            container.logger.info(`[${args.topic}] >> ${JSON.stringify(data)}\n`)
+            container.logger.info(`WS[${args.topic}] >> ${JSON.stringify(data)}\n`)
         })
     }
 }
